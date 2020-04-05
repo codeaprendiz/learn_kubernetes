3464d53c917be815305e788d0aa93ba093134035
codeaprendiz <codeaprendiz@gmail.com>
2020-04-05 17:16:56 +0400

    chore: update

--- gcp/task2-k8s-rbac/admin-run.sh
+++ gcp/task2-k8s-rbac/admin-run.sh
@@ -30,11 +30,11 @@ export CLUSTER_NAME=$(kubectl config view --minify -o jsonpath={.current-context
 # Client certificate
 export CLIENT_CERTIFICATE_DATA=$(kubectl get csr mycsr -o jsonpath='{.status.certificate}')
 # Cluster Certificate Authority
-#export CLUSTER_CA=$(kubectl config view --raw -o json | jq -r '.clusters[] | select(.name == "'$(kubectl config current-context)'") | .cluster."certificate-authority-data"')
-export CLUSTER_CA=$(kubectl config view --raw -o json | jq -r '.clusters[].cluster."certificate-authority-data"')
+export CLUSTER_CA=$(kubectl config view --raw -o json | jq -r '.clusters[] | select(.name == "'$(kubectl config current-context)'") | .cluster."certificate-authority-data"')
+#export CLUSTER_CA=$(kubectl config view --raw -o json | jq -r '.clusters[].cluster."certificate-authority-data"')
 
 # API Server endpoint
-#export CLUSTER_ENDPOINT=$(kubectl config view --raw -o json | jq -r '.clusters[] | select(.name == "'$(kubectl config current-context)'") | .cluster."server"')
-export CLUSTER_ENDPOINT=$(kubectl config view --raw -o json | jq -r '.clusters[].cluster."server"')
+export CLUSTER_ENDPOINT=$(kubectl config view --raw -o json | jq -r '.clusters[] | select(.name == "'$(kubectl config current-context)'") | .cluster."server"')
+#export CLUSTER_ENDPOINT=$(kubectl config view --raw -o json | jq -r '.clusters[].cluster."server"')
 
 cat kubeconfig.tpl | envsubst > kubeconfig
